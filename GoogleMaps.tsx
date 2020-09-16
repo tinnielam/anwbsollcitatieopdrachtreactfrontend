b2df8022612fd33972d7ad362206f227693a0e11
tinnielam <tinlamngoc@gmail.com>
2020-09-16 23:55:33 +0200

    3 files modified

--- GoogleMaps.tsx
+++ GoogleMaps.tsx
@@ -1,11 +1,26 @@
-import React from "react";
-let map: google.maps.Map;
+import React, { useState } from 'react';
+import GoogleMapReact from 'google-map-react';
 
-function initMap(): void {
-  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
-  });
+const AnyReactComponent = ({text}: any) => <div>{text}</div>;
+
+const SimpleMap = (props: any) => {
+    const [center, setCenter] = useState({lat: 11.0168, lng: 76.9558 });
+    const [zoom, setZoom] = useState(11);
+    return (
+        <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyCVaY96z82QyROvA7BvgOLIZs_rtkWeD2A' }}
+          defaultCenter={center}
+          defaultZoom={zoom}
+        >
+          <AnyReactComponent
+            lat={11.0168}
+            lng={76.9558}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+    );
 }
-// [END maps_map_simple]
-export default initMap;
+
+export default SimpleMap;
