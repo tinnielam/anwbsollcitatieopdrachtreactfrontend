878b1133ebd2eb2383490e03e723283dd24cfdb1
tinnielam <tinlamngoc@gmail.com>
2020-09-23 09:17:47 +0200

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import "./VerkeersinformatieJams.css";
-import Data from './VerkeersinformatieJams';
+import Jams from './VerkeersinformatieJams';
 import Map from './GoogleMaps';
 
 
 render(<Map />, document.getElementById("map"));
-render(<Data />, document.getElementById("root"));
+render(<Jams />, document.getElementById("root"));
