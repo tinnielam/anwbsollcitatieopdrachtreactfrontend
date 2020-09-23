7051dabcfbdea72eb127b516d748dcc212858262
tinnielam <tinlamngoc@gmail.com>
2020-09-23 10:35:40 +0200

    1 file modified, 4 added

---
+++ Verkeersinformatie/Radars/VerkeersinformatieRadars.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+
+class VerkeersinformatieRadars extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      verkeersinformatie: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://anwbtinlam.tinlam.repl.co/verkeersinformatieradars")
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            verkeersinformatie: result
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+  renderTableData(): JSX.Element {
+    return this.state.verkeersinformatie.map(verkeersinformatie =>
+      verkeersinformatie.segments.map(segments =>
+        segments.radars.map((key, index) => (
+          <tr key={key.id}>
+            <td>{key.road}</td>
+            <td>
+              {segments.start + " =>"} {segments.end}
+            </td>
+            <td>
+              {key.from + " =>"} {key.to}
+            </td>
+            <td>{key.reason}</td>
+            <td>{key.distance / 1000 + " KM"}</td>
+            <td>{key.delay / 60}</td>
+          </tr>
+        ))
+      )
+    );
+  }
+
+  renderTableHeader(): JSX.Element {
+    return (
+      <tr>
+        <th>Rijksweg</th>
+        <th>Traject</th>
+        <th>Route</th>
+        <th>Reden</th>
+        <th>Aantal KM</th>
+        <th>Tijd</th>
+      </tr>
+    );
+  }
+
+  render(): JSX.Element {
+    return (
+      <div>
+        <h1 id="title">Actuele verkeersinformatie</h1>
+        <table id="verkeersinformatieRadars">
+          <tbody>
+            {this.renderTableHeader()}
+            {this.renderTableData()}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default VerkeersinformatieRadars;
