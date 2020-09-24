240a5122eb529d91f302940439f23f8f7472bf32
tinnielam <tinlamngoc@gmail.com>
2020-09-24 12:06:18 +0200

    1 file modified, 1 added

--- GoogleMaps/GoogleMaps.tsx
+++ GoogleMaps/GoogleMaps.tsx
@@ -8,18 +8,41 @@ class GoogleMaps extends React.Component {
     super(props);
     this.state = {
       error: null,
-      isLoaded: false,
-      verkeersinformatie: []
+      verkeersinformatie: [],
+      mapsLoaded: false,
+      map: null,
+      maps: null
     };
   }
 
-  static defaultProps = {
-    center: {
-      lat: 52.254709,
-      lng: 5.353826
-    },
-    zoom: 8
-  };
+  onMapLoaded(map, maps) {
+    this.fitBounds(map, maps);
+    this.setState({     
+      mapsLoaded: true,
+      map: map,
+      maps: maps
+    });
+  }
+
+  fitBounds(map, maps) {
+    var bounds = new maps.LatLngBounds();
+    for (let marker of this.props.markers) {
+      bounds.extend(new maps.LatLng(marker.lat, marker.lng));
+    }
+    map.fitBounds(bounds);
+  }
+
+  afterMapLoadChanges() {
+    return (
+      <div style={{ display: "none" }}>
+        <Polyline
+          map={this.state.map}
+          maps={this.state.maps}
+          markers={this.props.markers}
+        />
+      </div>
+    );
+  }
 
   public componentDidMount(): void {
     const anwbData = new AnwbData();
@@ -35,6 +58,8 @@ class GoogleMaps extends React.Component {
           bootstrapURLKeys={{ key: "AIzaSyCVaY96z82QyROvA7BvgOLIZs_rtkWeD2A" }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
+          onGoogleApiLoaded={({ map, maps }) => this.onMapLoaded(map, maps)}
+                  {this.state.mapsLoaded ? this.afterMapLoadChanges() : ''}
         >
           {this.state.verkeersinformatie.map(verkeersinformatie =>
             verkeersinformatie.segments.map(segments =>
@@ -42,8 +67,20 @@ class GoogleMaps extends React.Component {
                 <Marker
                   lat={key.fromLoc.lat}
                   lng={key.fromLoc.lon}
-                  text="My Marker"
+                  text={key.events.text}
                   color="red"
+                />
+              ))
+            )
+          )}
+          {this.state.verkeersinformatie.map(verkeersinformatie =>
+            verkeersinformatie.segments.map(segments =>
+              segments.jams.map((key, index) => (
+                <Marker
+                  lat={key.toLoc.lat}
+                  lng={key.toLoc.lon}
+                  text={key.events.text}
+                  color="blue"
                 />
               ))
             )
@@ -52,6 +89,13 @@ class GoogleMaps extends React.Component {
       </div>
     );
   }
+  static defaultProps = {
+    center: {
+      lat: 52.254709,
+      lng: 5.353826
+    },
+    zoom: 8
+  };
 }
 
 export default GoogleMaps;
