7a4550443d49950ebe5d90e8454630fa624dfd4b
tinnielam <tinlamngoc@gmail.com>
2020-09-24 12:35:47 +0200

    1 file modified, 1 deleted

--- GoogleMaps/GoogleMaps.tsx
+++ GoogleMaps/GoogleMaps.tsx
@@ -8,47 +8,55 @@ class GoogleMaps extends React.Component {
     super(props);
     this.state = {
       error: null,
-      verkeersinformatie: [],
-      mapsLoaded: false,
-      map: null,
-      maps: null
+      verkeersinformatie: []
     };
   }
-
-  onMapLoaded(map, maps) {
-    this.fitBounds(map, maps);
-    this.setState({     
-      mapsLoaded: true,
-      map: map,
-      maps: maps
-    });
-  }
-
-  fitBounds(map, maps) {
-    var bounds = new maps.LatLngBounds();
-    for (let marker of this.props.markers) {
-      bounds.extend(new maps.LatLng(marker.lat, marker.lng));
-    }
-    map.fitBounds(bounds);
-  }
-
-  afterMapLoadChanges() {
-    return (
-      <div style={{ display: "none" }}>
-        <Polyline
-          map={this.state.map}
-          maps={this.state.maps}
-          markers={this.props.markers}
-        />
-      </div>
-    );
-  }
-
   public componentDidMount(): void {
     const anwbData = new AnwbData();
     anwbData
       .getAnwbData("jams")
       .then(data => this.setState({ verkeersinformatie: data }));
+  }
+
+  private getFromLocation() {
+    return this.state.verkeersinformatie.map(verkeersinformatie =>
+      verkeersinformatie.segments.map(segments =>
+        segments.jams.map((key, index) => (
+          <Marker
+            lat={key.fromLoc.lat}
+            lng={key.fromLoc.lon}
+            text={key.events.text}
+            color="red"
+          />
+        ))
+      )
+    );
+  }
+
+  private getToLocation() {
+    return this.state.verkeersinformatie.map(verkeersinformatie =>
+      verkeersinformatie.segments.map(segments =>
+        segments.jams.map((key, index) => (
+          <Marker
+            lat={key.toLoc.lat}
+            lng={key.toLoc.lon}
+            text={key.events.text}
+            color="blue"
+          />
+        ))
+      )
+    );
+  }
+
+    private renderPolylines(map, maps): any {
+    let geodesicPolyline = new maps.Polyline({
+      path: this.props.verkeersinformatie,
+      geodesic: true,
+      strokeColor: "#00a1e1",
+      strokeOpacity: 1.0,
+      strokeWeight: 4
+    });
+    geodesicPolyline.setMap(map);
   }
 
   render() {
@@ -58,33 +66,10 @@ class GoogleMaps extends React.Component {
           bootstrapURLKeys={{ key: "AIzaSyCVaY96z82QyROvA7BvgOLIZs_rtkWeD2A" }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
-          onGoogleApiLoaded={({ map, maps }) => this.onMapLoaded(map, maps)}
-                  {this.state.mapsLoaded ? this.afterMapLoadChanges() : ''}
+          onGoogleApiLoaded={({ map, maps }) => this.renderPolylines(map, maps)}
         >
-          {this.state.verkeersinformatie.map(verkeersinformatie =>
-            verkeersinformatie.segments.map(segments =>
-              segments.jams.map((key, index) => (
-                <Marker
-                  lat={key.fromLoc.lat}
-                  lng={key.fromLoc.lon}
-                  text={key.events.text}
-                  color="red"
-                />
-              ))
-            )
-          )}
-          {this.state.verkeersinformatie.map(verkeersinformatie =>
-            verkeersinformatie.segments.map(segments =>
-              segments.jams.map((key, index) => (
-                <Marker
-                  lat={key.toLoc.lat}
-                  lng={key.toLoc.lon}
-                  text={key.events.text}
-                  color="blue"
-                />
-              ))
-            )
-          )}
+          {this.getFromLocation()}
+          {this.getToLocation()}
         </GoogleMapReact>
       </div>
     );
